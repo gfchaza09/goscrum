730dce6ee6b593b4946549192e44c001d1749d36
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-06-23 00:48:56 -0300

    feat: basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './index.css';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="goscrum/">
       <Provider store={store} >
         <App />
       </Provider>
