64a7d5e4163e8f79acbd7ef023572e9dfbf26564
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-06-23 00:54:23 -0300

    config file: package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from './store/store';
@@ -12,11 +12,11 @@ import './index.css';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="goscrum/">
+    <HashRouter>
       <Provider store={store} >
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
